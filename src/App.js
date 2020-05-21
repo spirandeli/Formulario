9ea631b61d26c9c91f268357c5124339f2edb300
yurispirandeli <spirandelidev@gmail.com>
2020-05-21 18:01:22 -0300

    21-05-2020 18:01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Uf from "./Component/Cep"
+import  {Container, Linha, Coluna} from "./Component/Formulario";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id="form">
+        <Container>
+          <Linha>
+            <Coluna mobile='6' tablet='12' desktop='6'>
+                <label> Nome </ label>
+                <input name = "name" className = "controle de formulário"
+                       id = "name" placeholder = "Nome Completo"/> </Coluna>
+
+              <Coluna mobile='6' tablet='12' desktop='6'>
+                  <label> Data de Nascimento </ label>
+                  <input type="date" id="start" name="dataNascimento"
+                value="1998-03-14" min="1910-01-01" max="2005-01-01"/></Coluna>
+
+              <Coluna mobile='6' tablet='12' desktop='6'>
+                  <label> Cpf </ label>
+                  <input type="number" name = "cpf" id = "cpf" placeholder = "Digite o seu cpf"/></Coluna>
+          </Linha>
+            <Linha>
+                <Coluna mobile='6' tablet='12' desktop='6'>
+                    <label> Cep </ label>
+                    <input type="number" name = "cep"
+                           id = "cep" placeholder = "Cep"/> </Coluna>
+                <Coluna mobile='6' tablet='12' desktop='6'>
+                    <label> Estado </ label>
+                    <Uf /></Coluna>
+
+                <Coluna mobile='6' tablet='12' desktop='6'>
+                    <label> Cidade </ label>
+                    <input name = "cidade" id = "cidade" /></Coluna>
+            </Linha>
+            <Linha>
+                <Coluna mobile='6' tablet='12' desktop='6'>
+                    <label> Bairro </ label>
+                    <input name = "bairro" id = "bairro" /> </Coluna>
+
+                <Coluna mobile='6' tablet='12' desktop='6'>
+                    <label> Complemento </ label>
+                    <input id="complemento" name="complemento"/></Coluna>
+
+                <Coluna mobile='6' tablet='12' desktop='6'>
+                    <label> Numero </ label>
+                    <input type="number" name = "numero" id = "numero"/></Coluna>
+            </Linha>
+            <Linha>
+                <Coluna mobile='6' tablet='12' desktop='6'>
+                    <label> Telefone Celular </ label>
+                    <input type="number" name = "numeroCelular" id = "numeroCelular"/></Coluna>
+
+                <Coluna mobile='6' tablet='12' desktop='6'>
+                    <label> Telefone Residencial </ label>
+                    <input type="number" name = "numeroResidencial" id = "numeroResidencial"/></Coluna>
+            </Linha>
+        </Container>
+      </div>
   );
 }
 
