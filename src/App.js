1acf53921793b144e076173462f27a403dcc399d
yurispirandeli <spirandelidev@gmail.com>
2020-05-26 12:00:08 -0300

    Cep incompleto e useState

--- src/App.js
+++ src/App.js
@@ -1,64 +1,75 @@
 import React,{useState} from 'react';
-import Uf from "./Component/Cep"
+import Uf from "./Component/UF"
 import  {Container, Row, Column} from "./Component/Formulario";
 import './App.css'
+import Cep from "./Component/Cep";
+import useForm from "./Hooks/useForm";
+
 
 function App() {
-  return (
-        <form>
-        <Container>
-          <Row>
-            <Column mobile='6' tablet='12' desktop='6'>
-                <label> Nome </ label>
-                <input name = "name" className = "controle de formulário"
-                       id = "name" placeholder = "Nome Completo"/> </Column>
 
-              <Column mobile='6' tablet='12' desktop='6'>
-                  <label> Data de Nascimento </ label>
-                  <input type="date" id="start" name="dataNascimento"
-                value="1998-03-14" min="1910-01-01" max="2005-01-01"/></Column>
+    const [{ values, loading }, handleChange, handleSubmit] = useForm();
 
-              <Column mobile='6' tablet='12' desktop='6'>
-                  <label> Cpf </ label>
-                  <input type="number" name = "cpf" id = "cpf" placeholder = "Digite o seu cpf"/></Column>
-          </Row>
-            <Row>
-                <Column mobile='6' tablet='12' desktop='6'>
-                    <label> Cep </ label>
-                    <input type="number" name = "cep"
-                           id = "cep" placeholder = "Cep"/> </Column>
-                <Column mobile='6' tablet='12' desktop='6'>
-                    <label> Estado </ label>
-                    <Uf /></Column>
+    const enviarContato = () => {
+        console.log(values);
+    }
 
-                <Column mobile='6' tablet='12' desktop='6'>
-                    <label> Cidade </ label>
-                    <input name = "cidade" id = "cidade" /></Column>
-            </Row>
-            <Row>
-                <Column mobile='6' tablet='12' desktop='6'>
-                    <label> Bairro </ label>
-                    <input name = "bairro" id = "bairro" /> </Column>
 
-                <Column mobile='6' tablet='12' desktop='6'>
-                    <label> Complemento </ label>
-                    <input id="complemento" name="complemento"/></Column>
+    return (
+        <form method="get" action="." onSubmit={handleSubmit(enviarContato)}>
+        <div>
+            <Container />
+              <Row>
+                    <Column mobile='6' tablet='12' desktop='6'>
+                        <label> Nome </ label>
+                        <input onChange={handleChange} name = "name" className = "controle de formulário"
+                               id = "name" placeholder = "Nome Completo"/>
+                    </Column>
 
-                <Column mobile='6' tablet='12' desktop='6'>
-                    <label> Numero </ label>
-                    <input type="number" name = "numero" id = "numero"/></Column>
-            </Row>
-            <Row>
-                <Column mobile='6' tablet='12' desktop='6'>
-                    <label> Telefone Celular </ label>
-                    <input type="number" name = "numeroCelular" id = "numeroCelular"/></Column>
+                    <Column mobile='6' tablet='12' desktop='6'>
+                          <label> Data de Nascimento </ label>
+                        <input onChange={handleChange} type="date" id="start" name="dataNascimento"
+                        value="1998-03-14" min="1910-01-01" max="2005-01-01"/>
+                    </Column>
 
-                <Column mobile='6' tablet='12' desktop='6'>
-                    <label> Telefone Residencial </ label>
-                    <input type="number" name = "numeroResidencial" id = "numeroResidencial"/></Column>
-            </Row>
-            <input type="submit" name="Enviar"/>
-        </Container>
+                  <Column mobile='6' tablet='12' desktop='6'>
+                      <label> Cpf </ label>
+                      <input onChange={handleChange} type="number" name = "cpf" id = "cpf" placeholder = "Digite o seu cpf"/>
+                  </Column>
+                  </Row>
+
+                <Row>
+                <Cep />
+                </Row>
+                <Row>
+
+                    <Column mobile='6' tablet='12' desktop='6'>
+                        <label> Bairro </ label>
+                        <input onChange={handleChange} name = "bairro" id = "bairro" />
+                    </Column>
+
+
+                    <Column mobile='6' tablet='12' desktop='6'>
+                        <label> Complemento </ label>
+                        <input onChange={handleChange} id="complemento" name="complemento"/>
+                    </Column>
+
+                        <Column mobile='6' tablet='12' desktop='6'>
+                            <label> Numero </ label>
+                            <input onChange={handleChange} type="number" name = "numero" id = "numero"/>
+                        </Column>
+                </Row>
+                <Row>
+                    <Column mobile='6' tablet='12' desktop='6'>
+                        <label> Telefone Celular </ label>
+                        <input onChange={handleChange} type="number" name = "numeroCelular" id = "numeroCelular"/></Column>
+
+                    <Column mobile='6' tablet='12' desktop='6'>
+                        <label> Telefone Residencial </ label>
+                        <input onChange={handleChange} type="number" name = "numeroResidencial" id = "numeroResidencial"/></Column>
+                </Row>
+                <input type="submit" name="Enviar"/>
+        </div>
         </form>
   );
 }
--- src/Component/Cep/index.js
+++ src/Component/Cep/index.js
@@ -1,38 +1,94 @@
 import React from "react";
+import Axios from "axios";
+import {Column, Row} from "../Formulario";
+import Uf from "../UF";
 
-function Uf ()  {
-    return (
-    <select id="estado" name="estado">
-        <option value="AC">Acre</option>
-        <option value="AL">Alagoas</option>
-        <option value="AP">Amapá</option>
-        <option value="AM">Amazonas</option>
-        <option value="BA">Bahia</option>
-        <option value="CE">Ceará</option>
-        <option value="DF">Distrito Federal</option>
-        <option value="ES">Espírito Santo</option>
-        <option value="GO">Goiás</option>
-        <option value="MA">Maranhão</option>
-        <option value="MT">Mato Grosso</option>
-        <option value="MS">Mato Grosso do Sul</option>
-        <option value="MG">Minas Gerais</option>
-        <option value="PA">Pará</option>
-        <option value="PB">Paraíba</option>
-        <option value="PR">Paraná</option>
-        <option value="PE">Pernambuco</option>
-        <option value="PI">Piauí</option>
-        <option value="RJ">Rio de Janeiro</option>
-        <option value="RN">Rio Grande do Norte</option>
-        <option value="RS">Rio Grande do Sul</option>
-        <option value="RO">Rondônia</option>
-        <option value="RR">Roraima</option>
-        <option value="SC">Santa Catarina</option>
-        <option value="SP">São Paulo</option>
-        <option value="SE">Sergipe</option>
-        <option value="TO">Tocantins</option>
-        <option value="EX">Estrangeiro</option>
-    </select>
-    )
+function Cep () {
+
+    function limpa_formulário_cep() {
+        //Limpa valores do formulário de cep.
+        document.getElementById('rua').value=("");
+        document.getElementById('bairro').value=("");
+        document.getElementById('cidade').value=("");
+        document.getElementById('uf').value=("");
+        document.getElementById('ibge').value=("");
+    }
+
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('rua').value=(conteudo.logradouro);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.localidade);
+            document.getElementById('uf').value=(conteudo.uf);
+            document.getElementById('ibge').value=(conteudo.ibge);
+        } //end if.
+        else {
+            //CEP não Encontrado.
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+
+    function pesquisacep(valor) {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = valor.replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                document.getElementById('rua').value="...";
+                document.getElementById('bairro').value="...";
+                document.getElementById('cidade').value="...";
+                document.getElementById('uf').value="...";
+                document.getElementById('ibge').value="...";
+
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    };
+
+    return(
+        <form method="get" action=".">
+
+            <Column>
+            <label>Cep </label>
+                <input name="cep" type="text" id="cep" value="" size="10" maxLength="9"
+                       onBlur="pesquisacep(this.value);"/>
+            </Column>
+            <Column>
+            <label> Cidade </label>
+                <input name="cidade" type="text" id="cidade" size="40"/>
+            </Column>
+            <Column>
+                <label>Estado </label>
+                <Uf/> </Column>
+        </form>)
 }
 
-export default Uf;
+export default Cep;
